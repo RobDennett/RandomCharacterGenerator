be393d19389d2aabf2aeeed94075a3bd06bcd554
RobDennett <rob_dennett@live.com>
2021-01-12 22:30:45 +0000

    Test

--- RandomCharacterGen/GetInfo.cs
+++ RandomCharacterGen/GetInfo.cs
@@ -7,13 +7,25 @@ namespace RandomCharacterGen
     {
         public static void CharacterInfo(string requestInfo, List<string> list)
         {
-            string addAnother;
+            string addAnother = "";
             do
             {
                 Console.Write(requestInfo);
-                list.Add(Console.ReadLine());
-                Console.Write("Add another? y/n: ");
-                addAnother = Console.ReadLine();
+                string userInput = Console.ReadLine();
+
+                if (userInput != "")
+                {
+                    list.Add(userInput);
+                    Console.Write("Add another? y/n: ");
+                    addAnother = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("That was an invalid. Please try again.");
+                    addAnother = "y";
+                    Console.WriteLine();
+                }
 
             } while (addAnother.ToLower() == "y");
 
